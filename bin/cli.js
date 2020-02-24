391e0af039373fb3ad49e565232d9e4ecaa97007
sky <cl98914@alibaba-inc.com>
2020-02-24 23:07:21 +0800

    fix: npm bin file missing and start to run

--- bin/cli.js
+++ bin/cli.js
@@ -8,7 +8,7 @@ const cliFilePath = path.join(process.cwd(), 'node_modules', '@easy-team/res-cli
 // fix when production mode, ves-cli not exists, start app error by ves start
 if (fs.existsSync(cliFilePath)) {
   const ResCLI = require(cliFilePath);
-  new ResCLI.Command().start();
+  new ResCLI.Command().run();
 } else {
   new EggScriptCommand().start();
 }
